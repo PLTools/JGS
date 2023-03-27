2e8a88387be229f5a2c90c85dff06af62e378f0d
Dmitry Boulytchev <dboulytchev@gmail.com>
2023-03-28 00:11:02 +0300

    First encoding for types

--- src/JGS.noc.ml
+++ src/JGS.noc.ml
@@ -1,1 +1,26 @@
-let _ = []
+open List
+open Peano
+  
+type id = A | B | C (* should be generated from a class table *)
+
+(* Class or interface declaration *)                
+type cidecl = {
+  id     : id;          (* identifier *)
+  parms  : bound array; (* type parameters:
+                             1. type parameters are represented implicitly by their indices;
+                             2. the number of parameters is the length of the bounds' array.
+                         *)
+  super_c: jtype;       (* superclass      *)
+  super_i: jtype array  (* superinterfaces *)
+  }
+(* Java types; only reference types since
+   primitive types seem to be not involved
+*)            
+and jtype =
+| Array     of jtype
+| Class     of id * targ array  (* class type     *)
+| Interface of id * targ array  (* interface type *)
+| Var       of int              (* type variable  *)
+and bound    = jtype array
+and targ     = Type of jtype | Wildcard of polarity * jtype
+and polarity = Extends | Super

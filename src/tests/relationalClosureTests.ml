49bf59cfe879f3fac8e31fb121fc95f55ae31167
Peter Lozov <not_kill@gmail.ru>
2023-05-24 18:50:04 +0300

    Added simple well-formed types checker.

--- src/tests/relationalClosureTests.ml
+++ src/tests/relationalClosureTests.ml
@@ -7,13 +7,26 @@ open JGS_lib.MutableTypeTable
 let _ =
   let module SampleCT = SampleCT () in
   let module V = FO.Verifier (SampleCT) in
-  let rec ( <-< ) ta tb b =
+  let rec is_correct_type t =
     conde
       [
-        fresh () (b === !!true) (ta === tb);
-        fresh ti ((tb -<- ti) b) ((ti <-< ta) b);
+        fresh (id index upb lwb)
+          (t === !!(HO.Var { id; index; upb; lwb = some lwb }))
+          (lwb -<- upb);
+        fresh (id index upb) (t === !!(HO.Var { id; index; upb; lwb = none () }));
+        ( wc @@ fun id ->
+          wc @@ fun index ->
+          wc @@ fun upb ->
+          wc @@ fun lwb -> t =/= !!(HO.Var { id; index; upb; lwb }) );
       ]
-  and ( -<- ) ta tb b = V.( -<- ) ( <-< ) ta tb b in
+  and ( <-< ) ta tb b =
+    fresh () (b === !!true)
+      (conde [ ta === tb; fresh ti (tb -<- ti) ((ti <-< ta) b) ])
+  and ( -<- ) ta tb =
+    fresh ()
+      (V.( -<- ) ( <-< ) ta tb !!true)
+      (is_correct_type ta) (is_correct_type tb)
+  in
 
   let _run_jtype ?(n = -1) query =
     let pp_list f l =
@@ -39,7 +52,7 @@ let _ =
   Printf.printf "Class A: %d\n\n" class_a;
 
   let class_a1 = SampleCT.make_class [] SampleCT.object_t [] in
-  let a1 = Class (class_a1, []) in
+  let _a1 = Class (class_a1, []) in
   Printf.printf "Class A1: %d\n\n" class_a1;
 
   let class_b = SampleCT.make_class [] a [] in
@@ -47,14 +60,13 @@ let _ =
   Printf.printf "Class B: %d\n\n" class_b;
 
   let class_c = SampleCT.make_class [] b [] in
-  let _c = Class (class_c, []) in
+  let c = Class (class_c, []) in
   Printf.printf "Class C: %d\n\n" class_c;
 
   Printf.printf "%s\n"
   @@ run_jtypes ~n:1 (fun q ->
-         fresh (super sub t1 t2 t3)
+         fresh (super sub t1)
            (super === jtype_inj a)
-           (sub === jtype_inj a1)
-           (q === Std.list Fun.id [ super; t1; t2; t3; sub ])
-           ((t1 -<- super) !!true)
-           ((sub -<- t1) !!true))
+           (sub === jtype_inj c)
+           (q === Std.list Fun.id [ super; t1; sub ])
+           (t1 -<- super) (sub -<- t1))
--- src/tests/relationalClosureTests.t
+++ src/tests/relationalClosureTests.t
@@ -9,14 +9,5 @@
   
   
   [
-    Class (4, []);
-    _.93;
-    Var {id=_.76, index=_.77, upb=Class (4, []), lwb=_.79};
-    Null;
-    Intersect ([Class (4, []) | _.201]);
-    Intersect ([_.203 [=/= Class (4, [])]; Class (4, []) | _.344]);
-    Intersect ([_.203 [=/= Class (4, [])]; _.346 [=/= Class (4, [])]; Class (4, []) | _.418]);
-    Intersect ([_.203 [=/= Class (4, [])]; _.346 [=/= Class (4, [])]; _.420 [=/= Class (4, [])]; Class (4, []) | _.462]);
-    Class (_.145, _.146);
-    Intersect ([_.203 [=/= Class (4, [])]; _.346 [=/= Class (4, [])]; _.420 [=/= Class (4, [])]; _.464 [=/= Class (4, [])]; Class (4, []) | _.507])
+    [Class (4, []); Class (6, []); Class (7, [])]
   ]

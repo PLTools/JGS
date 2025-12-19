69e0c8bd69de0dad80469a427c07c6234ce640d3
Kakadu <Kakadu@pm.me>
2025-12-19 17:23:25 +0300

    CI
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- src/jtype_set.mli
+++ src/jtype_set.mli
@@ -1,5 +1,7 @@
 type t
 
+val empty : t
+val cardinal : t -> int
 val alpha_converted_answer_set : t ref
 val add_alpha_converted : JGS.HO.jtype_logic -> t -> t
 val mem_alpha_converted : JGS.HO.jtype_logic -> t -> bool
